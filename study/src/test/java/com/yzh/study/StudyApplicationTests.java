6e91554a57f9c5c427badd673ec1207bb3b12562
Yangzhehui <yangzhe.hui@qq.com>
2023-11-24 13:52:13 +0800

    modified by idea

--- study/src/test/java/com/yzh/study/StudyApplicationTests.java
+++ study/src/test/java/com/yzh/study/StudyApplicationTests.java
@@ -12,7 +12,7 @@ class StudyApplicationTests {
 		System.out.println("----------------");
 		System.out.println("----------------");
 		System.out.println("------github 修改文件内容----");
-		System.out.println("----------------");
+		System.out.println("-------idea 修改文件内容---------");
 		System.out.println("----------------");
 	}
 
